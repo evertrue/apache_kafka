697a8f5b54b2f008c5a104a90097ffba66a9e4b4
Eric Herot <eric.github@herot.com>
2016-09-08 17:14:33 -0400

    Bump version to 3.0.0 and update maintainer information

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
 # encoding: UTF-8
 # rubocop:disable SingleSpaceBeforeFirstArg, LineLength
 name             "et_apache_kafka"
-maintainer       "Daniel Couture"
-maintainer_email "mathyourlife@gmail.com"
+maintainer       "EverTrue, Inc"
+maintainer_email "devops@evertrue.com"
 license          "Apache-2.0"
 description      "Installs/Configures Apache Kafka >= 0.7.0"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "2.0.0"
+version          "3.0.0"
 
 %w{ ubuntu debian }.each do |os|
   supports os
